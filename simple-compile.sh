cd69083945d67bc105316e57a56b452f0be18bea
ambrosa <aleambro@gmail.com>
2014-11-26 18:43:46 +0100

    added extrapatch dir

--- simple-compile.sh
+++ simple-compile.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # http://wiki.openwrt.org/doc/howto/build
-
+rm -f build.log
+date >> build.log
+make clean
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 make menuconfig
 make defconfig
-make V=s 2>&1 | tee build.log
+make V=s 2>&1 | tee -a build.log
+date >> build.log
 
